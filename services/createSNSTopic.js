a92bf043d777afe04b42297c56e2953371f22169
Leonardo <dleonardotb2001@gmail.com>
2021-11-07 19:42:59 -0500

    fix: code smell createSNSTopic

--- services/createSNSTopic.js
+++ services/createSNSTopic.js
@@ -6,23 +6,20 @@ AWS.config.update({
     region: process.env.AWS_REGION
 });
 
-const createSNSTopic = async topicName => {
+const createSNSTopic = topicName => {
 
     // Create promise and SNS service object
     let createTopicPromise = new AWS.SNS({apiVersion: '2010-03-31'}).createTopic({Name: topicName}).promise();
 
     // Handle promise's fulfilled/rejected states
-    const data = createTopicPromise.then(
-        (data) => {
-            console.log("Topic ARN is " + data.TopicArn);
-            return data.TopicArn;
+    return createTopicPromise.then(
+        (payload) => {
+            console.log("Topic ARN is " + payload.TopicArn);
+            return payload.TopicArn;
     }).catch(
         (err) => {
             console.error(err, err.stack);
     });
-
-    const arn = await data;
-    return arn;
  
 }
 
